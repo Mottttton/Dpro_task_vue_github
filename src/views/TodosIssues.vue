5fa4e6c48697431d39154c4060b042b00a639ba7
Mottttton <mottttton.webdev@gmail.com>
2024-03-19 16:42:28 +0900

    preparation before task

---
+++ src/views/TodosIssues.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <h1>issueリスト</h1>
+    <form @submit.prevent="addTodo()">
+      <el-input placeholder="todo" v-model="todo"></el-input>
+    </form>
+    <div>
+      <TodoItem v-for="( todo, index ) in todos" :key="index" />
+      <el-col :span="12" v-for="( issue, index ) in issues" :key="issue.id">
+        <el-card class="box-card" shadow="hover" style="margin: 5px 0;">
+          <el-row :gutter="12">
+            <el-col :span="21">{{ issue.title }}</el-col>
+            <el-col :span="3">
+              <el-button @click="removeIssue(index)" type="success" icon="el-icon-check" circle></el-button>
+            </el-col>
+          </el-row>
+        </el-card>
+      </el-col>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import TodoItem from '@/components/TodoItem';
+
+const client = axios.create({
+  baseURL: `${process.env.VUE_APP_GITHUB_ENDPOINT}`,
+  headers: {
+    'Authorization': `token ${process.env.VUE_APP_GITHUB_TOKEN}`,
+    'Accept': 'application/vnd.github.v3+json',
+    'Content-Type':'application/json',
+  },
+})
+
+export default {
+  name: 'TodosIssues',
+  components: {
+    TodoItem
+  },
+  data() {
+    return {
+      todo: '',
+      todos: [],
+      issues: [],
+    }
+  },
+  methods: {
+    addTodo(){
+      this.todos.push(this.todo);
+      this.todo = '';
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1)
+    },
+    fetchIssue() {
+      client.get('/issues')
+      .then((res) => {
+        this.issues = res.data;
+      })
+    },
+    closeIssue(index) {
+      const target = this.issues[index]
+      client.patch(`/issues/${target.number}`,
+        {
+          state: 'closed'
+        },
+      ).then(() => {
+        this.issues.splice(index, 1)
+      })
+    },
+  },
+  created() {
+    this.fetchIssue();
+  }
+}
+</script>
